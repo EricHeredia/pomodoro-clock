b9c75890d23ddb28e14436910c2677a7f40c737e
EricHeredia <EricDavidHeredia@gmail.com>
2019-01-09 22:41:39 -0600

    Updates

--- precache-manifest.291764158197835cb4de6fbd903a71b8.js
+++ precache-manifest.291764158197835cb4de6fbd903a71b8.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
   },
   {
-    "revision": "23c20bd94209d9d2afc4",
-    "url": "/pomodoro-clock/static/js/main.23c20bd9.chunk.js"
+    "revision": "1daa6b0e77126bb605b5",
+    "url": "/pomodoro-clock/static/js/main.1daa6b0e.chunk.js"
   },
   {
     "revision": "15066001ad3c73e9227a",
     "url": "/pomodoro-clock/static/js/1.15066001.chunk.js"
   },
   {
-    "revision": "23c20bd94209d9d2afc4",
-    "url": "/pomodoro-clock/static/css/main.2e98ef39.chunk.css"
+    "revision": "1daa6b0e77126bb605b5",
+    "url": "/pomodoro-clock/static/css/main.71b2425d.chunk.css"
   },
   {
-    "revision": "ffc4d3432863020495bc70f1569ce328",
+    "revision": "43e3187296678667c65a47c9dc9c74a0",
     "url": "/pomodoro-clock/index.html"
   }
 ];
